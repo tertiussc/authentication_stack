fab84efba3dc5e3a513852fcfe1b845c306d7d35
Tertius <50881039+tertiussc@users.noreply.github.com>
2022-01-06 09:29:01 +0200

    Added helper functions to functions

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,63 @@
 <?php
-// General functions
+
+/********** Helper functions **********/
+
+/** Clean strings from inputs
+ * 
+ * @param string $string The string to be cleaned
+ * 
+ * @return string $string The cleaned string
+ */
+function clean($string)
+{
+    return htmlentities($string);
+}
+
+/** Redirect to page
+ * 
+ * @param string $location The url of page to redirect to
+ * 
+ * @return null Redirect to page specified
+ */
+function redirect($location)
+{
+    return header("Location: {$location}");
+}
+
+/** Set a messages in sessions
+ * 
+ * @param string $message Set the message that you want to be contained in sessions
+ * 
+ */
+function set_messages($message)
+{
+    if (!empty($message)) {
+        $_SESSION['message'] = $message;
+    } else {
+        $message = "";
+    }
+}
+
+/** Display message if is set in sessions
+ * @return string Session message
+ */
+function display_message()
+{
+    if (isset($_SESSION['message'])) {
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
+    };
+}
+
+/** Generate a token and encrypt it
+ * 
+ * @return string $token Set the token in sessions and retruns a unique and encrypted Token variable
+ */
+function token_generator()
+{
+    $token = $_SESSION['token'] = md5(uniqid(mt_rand(), true));
+    return $token;
+}
 
 
-
-?>
+/********** Validation functions **********/
