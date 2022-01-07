db6529eae15caf6cc7e521323dc66a0ded3196be
Tertius <50881039+tertiussc@users.noreply.github.com>
2022-01-07 10:31:30 +0200

    completed registration page validations

--- functions/functions.php
+++ functions/functions.php
@@ -59,5 +59,97 @@ function token_generator()
     return $token;
 }
 
+function email_exist($email)
+{
+    $sql = "SELECT id from users WHERE email = '$email'";
+
+    $result = query($sql);
+
+    if (row_count($result) == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function username_exist($username)
+{
+    $sql = "SELECT id from users WHERE username = '$username'";
+
+    $result = query($sql);
+
+    if (row_count($result) == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 /********** Validation functions **********/
+
+/** Validate registration fields: 
+ * Cleans input values. 
+ * Check to see if $first_name, $last_name and $username is between the minimum and maximum allowed characters
+ * @return string $error Returns error messages if any is found
+ */
+function validate_user_registration()
+{
+    $errors = [];
+    $min = 3;
+    $max = 25;
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $first_name = clean($_POST['first_name']);
+        $last_name = clean($_POST['last_name']);
+        $username =  clean($_POST['username']);
+        $email = clean($_POST['email']);
+        $password = clean($_POST['password']);
+        $confirm_password = clean($_POST['confirm_password']);
+    } else {
+        // Initialize values if the form is not submitted
+        $first_name = null;
+        $last_name = null;
+        $username = null;
+        $email = null;
+        $password = null;
+        $confirm_password = null;
+    }
+
+    if (strlen($first_name) <= $min) {
+        $errors[] = "First name must not be less then {$min} characters long";
+    }
+    if (strlen($last_name) <= $min) {
+        $errors[] = "Last name must not be less then {$min} characters long";
+    }
+    if (strlen($username) <= $min) {
+        $errors[] = "Username name must not be less then {$min} characters long";
+    }
+    if (strlen($first_name) >= $max) {
+        $errors[] = "First name must be not more then {$max} characters long";
+    }
+    if (strlen($last_name) >= $max) {
+        $errors[] = "Last name must be not more then {$max} characters long";
+    }
+    if (strlen($username) >= $max) {
+        $errors[] = "Username name must be not more then {$max} characters long";
+    }
+    if (username_exist($username)) {
+        $errors[] = "This username is already in use";
+    }
+
+    if ($password !== $confirm_password) {
+        $errors[] = "Your passwords do not match";
+    }
+
+    if (email_exist($email)) {
+        $errors[] = "This email is already in use";
+    }
+
+    // Echo errors
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo "<p class='callout-danger'>$error</p><br>";
+        }
+    }
+}
--- register.php
+++ register.php
@@ -1,15 +1,18 @@
 <?php include_once("includes/header.php") ?>
 <?php include_once("includes/navbar.php") ?>
-
+<?php
+// Initialize values
+$first_name = (isset($first_name)) ? $first_name : '';
+?>
 <div class="container">
-
 	<div class="row">
 		<div class="col-lg-6 offset-lg-3">
-</div>
-</div>
+			<?php validate_user_registration(); ?>
+		</div>
+	</div>
 	<div class="row">
 		<div class="col-md-6 offset-md-3 mt-5">
-			<div class="card panel-login">
+			<div class="card">
 				<div class="card-header">
 					<div class="row">
 						<div class="col-6">
@@ -25,6 +28,12 @@
 						<div class="col-lg-12">
 							<form id="register-form" method="post" role="form">
 
+								<div class="mb-3">
+									<input type="text" name="first_name" id="first_name" tabindex="1" class="form-control" placeholder="First Name" value="<?= $first_name;?>" required>
+								</div>
+								<div class="mb-3">
+									<input type="text" name="last_name" id="register_last_name" tabindex="1" class="form-control" placeholder="Last Name" value="" required>
+								</div>
 								<div class="mb-3">
 									<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="" required>
 								</div>
